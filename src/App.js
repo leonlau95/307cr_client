3c1c9b0350dc0eaf3fdb057777ad3c3c6a483621
unknown <leonlau95@gmail.com>
2018-11-26 22:15:31 +0800

    debug

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import './App.css';
 import axios from 'axios';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
-import DeletedMessage from './DeletedMessage';
 
 import {
   LineChart,
@@ -24,8 +23,7 @@ class App extends Component {
       sensor1alldata: [],
       errors: '',
       sensor2: 0,
-      sensor2alldata: [],
-      showDeletedMessage: false
+      sensor2alldata: []
     };
   }
 
@@ -82,190 +80,157 @@ class App extends Component {
     }, 5000);
   }
 
-  deleteAllHandler = () => {
-    axios
-      .get('https://glacial-citadel-67468.herokuapp.com/deleteall')
-      .then(result => {
-        this.setState({ showDeletedMessage: true });
-      })
-      .catch(err => {
-        console.log('unable to delete: ', err);
-      });
-  };
-
-  alertDismissHandler = () => {
-    this.setState({ showDeletedMessage: false });
-  };
-
-
   render() {
     return (
       <div>
-          <div className="jumbotron text-center header">
-            <h1>Sensors Data</h1>
-            <p>Displays sensors data received from NodeMCU</p>
-          </div>
+        <div className="jumbotron text-center header">
+          <h1>Sensors Data</h1>
+          <p>Displays sensors data received from NodeMCU</p>
+        </div>
 
-          <div className="container">
-            <DeletedMessage
-              show={this.state.showDeletedMessage}
-              alertDismiss={this.alertDismissHandler}
-            />
-            <div className="row">
-              <div className="text-center container col-sm-4">
-                <div className="row">
-                <div className="text-center container col-sm-6">
-                   <div id="deleteall">
-                     <br />
-                     <button
-                       className="btn btn-primary"
-                       onClick={this.deleteAllHandler}
-                     >
-                       <h4>Delete All</h4>
-                     </button>
-                   </div>
-                 </div>
-                 <div className="text-center container col-sm-6">
-                  <div className="sensorvalue">
-                    <label>
-                      <h4>Virbration Sensor Value</h4>
-                    </label>
-                    <br />
-                    <label>
-                      <h4>{this.state.sensor1} </h4>
-                    </label>
-                  </div>
-                </div>
-                <div>
-                  <label />
-                </div>
-                <div className="row">
-                  <div className="sensortable">
-                    <label>
-                      <h4>Vibration Sensor Table</h4>
-                    </label>
-                    <br />
-                    <div>
-                      <ReactTable
-                        data={this.state.sensor1alldata}
-                        columns={[
-                          {
-                            Header: 'Sequence',
-                            accessor: 'seq'
-                          },
-                          {
-                            Header: 'Value',
-                            accessor: 'value'
-                          }
-                        ]}
-                        defaultPageSize={5}
-                        className="-striped -highlight"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="text-center container col-sm-8">
-                <div className="sensorgraph">
+        <div className="container">
+          <div className="row">
+            <div className="text-center container col-sm-4">
+              <div className="row">
+                <div className="sensorvalue">
                   <label>
-                    <h4>Vibration Sensor Graph</h4>
+                    <h4>Virbration Sensor Value</h4>
                   </label>
                   <br />
-                  <LineChart
-                    width={700}
-                    height={400}
-                    data={this.state.sensor1alldata}
-                  >
-                    <Line type="monotone" dataKey="value" stroke="#8884d8" />
-                    <XAxis dataKey="seq">
-                      <Label position="insideBottomRight" dy={10}>
-                        Sequence
-                      </Label>
-                    </XAxis>
-                    <YAxis dataKey="value">
-                      <Label position="insideTopLeft" dx={-10}>
-                        Value
-                      </Label>
-                    </YAxis>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <Tooltip />
-                    <Legend />
-                  </LineChart>
+                  <label>
+                    <h4>{this.state.sensor1} </h4>
+                  </label>
+                </div>
+              </div>
+              <div>
+                <label />
+              </div>
+              <div className="row">
+                <div className="sensortable">
+                  <label>
+                    <h4>Vibration Sensor Table</h4>
+                  </label>
+                  <br />
+                  <div>
+                    <ReactTable
+                      data={this.state.sensor1alldata}
+                      columns={[
+                        {
+                          Header: 'Sequence',
+                          accessor: 'seq'
+                        },
+                        {
+                          Header: 'Value',
+                          accessor: 'value'
+                        }
+                      ]}
+                      defaultPageSize={5}
+                      className="-striped -highlight"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
+            <div className="text-center container col-sm-8">
+              <div className="sensorgraph">
+                <label>
+                  <h4>Vibration Sensor Graph</h4>
+                </label>
+                <br />
+                <LineChart
+                  width={700}
+                  height={400}
+                  data={this.state.sensor1alldata}
+                >
+                  <Line type="monotone" dataKey="value" stroke="#8884d8" />
+                  <XAxis dataKey="seq">
+                    <Label position="insideBottomRight" dy={10}>
+                      Sequence
+                    </Label>
+                  </XAxis>
+                  <YAxis dataKey="value">
+                    <Label position="insideTopLeft" dx={-10}>
+                      Value
+                    </Label>
+                  </YAxis>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip />
+                  <Legend />
+                </LineChart>
+              </div>
+            </div>
           </div>
-          <br></br>
-          <div className="container">
-            <div className="row">
-              <div className="text-center container col-sm-4">
-                <div className="row">
-                  <div className="sensorvalue">
-                    <label>
-                      <h4>Ultrasonic Sensor Value</h4>
-                    </label>
-                    <br />
-                    <label>
-                      <h4>{this.state.sensor2} </h4>
-                    </label>
-                  </div>
+        </div>
+        <br></br>
+        <div className="container">
+          <div className="row">
+            <div className="text-center container col-sm-4">
+              <div className="row">
+                <div className="sensorvalue">
+                  <label>
+                    <h4>Ultrasonic Sensor Value</h4>
+                  </label>
+                  <br />
+                  <label>
+                    <h4>{this.state.sensor2} </h4>
+                  </label>
                 </div>
-                <div>
-                  <label />
-                </div>
-                <div className="row">
-                  <div className="sensortable">
-                    <label>
-                      <h4>Ultrasonic Sensor Table</h4>
-                    </label>
-                    <br />
-                    <div>
-                      <ReactTable
-                        data={this.state.sensor2alldata}
-                        columns={[
-                          {
-                            Header: 'Sequence',
-                            accessor: 'seq'
-                          },
-                          {
-                            Header: 'Value',
-                            accessor: 'value'
-                          }
-                        ]}
-                        defaultPageSize={5}
-                        className="-striped -highlight"
-                      />
-                    </div>
+              </div>
+              <div>
+                <label />
+              </div>
+              <div className="row">
+                <div className="sensortable">
+                  <label>
+                    <h4>Ultrasonic Sensor Table</h4>
+                  </label>
+                  <br />
+                  <div>
+                    <ReactTable
+                      data={this.state.sensor2alldata}
+                      columns={[
+                        {
+                          Header: 'Sequence',
+                          accessor: 'seq'
+                        },
+                        {
+                          Header: 'Value',
+                          accessor: 'value'
+                        }
+                      ]}
+                      defaultPageSize={5}
+                      className="-striped -highlight"
+                    />
                   </div>
                 </div>
               </div>
-              <div className="text-center container col-sm-8">
-                <div className="sensorgraph">
-                  <label>
-                    <h4>Ultrasonic Sensor Graph</h4>
-                  </label>
-                  <br />
-                  <LineChart
-                    width={700}
-                    height={400}
-                    data={this.state.sensor2alldata}
-                  >
-                    <Line type="monotone" dataKey="value" stroke="#8884d8" />
-                    <XAxis dataKey="seq">
-                      <Label position="insideBottomRight" dy={10}>
-                        Sequence
-                      </Label>
-                    </XAxis>
-                    <YAxis dataKey="value">
-                      <Label position="insideTopLeft" dx={-10}>
-                        Value
-                      </Label>
-                    </YAxis>
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <Tooltip />
-                    <Legend />
-                  </LineChart>
-                </div>
+            </div>
+            <div className="text-center container col-sm-8">
+              <div className="sensorgraph">
+                <label>
+                  <h4>Ultrasonic Sensor Graph</h4>
+                </label>
+                <br />
+                <LineChart
+                  width={700}
+                  height={400}
+                  data={this.state.sensor2alldata}
+                >
+                  <Line type="monotone" dataKey="value" stroke="#8884d8" />
+                  <XAxis dataKey="seq">
+                    <Label position="insideBottomRight" dy={10}>
+                      Sequence
+                    </Label>
+                  </XAxis>
+                  <YAxis dataKey="value">
+                    <Label position="insideTopLeft" dx={-10}>
+                      Value
+                    </Label>
+                  </YAxis>
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <Tooltip />
+                  <Legend />
+                </LineChart>
               </div>
             </div>
           </div>
